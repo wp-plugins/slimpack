427eb50a91b9c692495d19e75b2e429b1b5f2f73
Sparanoid <Sparanoid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-08 10:38:48 +0000

    Preparing for 1.0.0 release
    git-svn-id: https://plugins.svn.wordpress.org/slimpack/trunk@1156023 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,147 @@
+=== Slimpack ===
+
+Contributors: Sparanoid
+Donate link: http://sparanoid.com/donate/
+Tags: api, archives, audio, Blip, blip.tv, dailymotion, Digg, equations, flickr, gallery, google, googlemaps, googlevideo, gravatar, hovercards, jet pack, json, kyte, kytetv, latex, livevideo, math, maths, monitor, mosaic, notes, notifications, omnisearch, profile, redlasso, REST, rockyou, rss, scribd, search, shortcode, shortlinks, slide, slideshare, slideshow, soundcloud, sso, statistics, stats, subscriptions, tweets, twitter, videopress, views, vimeo, widget, WordPress.com, wp.me, youtube
+Requires at least: 4.1
+Tested up to: 4.2.1
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Slimpack — Lightweight Jetpack. Super-fast performance without modules that requires contracting WordPress.com.
+
+== Description ==
+
+Slimpack — Lightweight Jetpack. Super-fast performance without modules that requires contracting WordPress.com.
+
+It provides better performance than original Jetpack. All features that require a WordPress.com account or need contracting / syncing back to WordPress.com servers have been removed. You don't need to connect to WordPress.com to use this plugin. If your site got slow response time (TTFB) after activating the original Jetpack, you should definitely try out this plugin.
+
+More information please visit my [site](http://sparanoid.com/work/slimpack/).
+
+View source code and submit issue at [GitHub](https://github.com/sparanoid/slimpack)
+
+Note: Please do not submit issue unless the issue cannot be reproduced using the original Jetpack module.
+
+**Features**
+
+* All source code is untouched and synced from Jetpack
+* Better performance than original Jetpack. All features that require a WordPress.com account have been removed. You don't need to connect to WordPress.com to use this plugin.
+* Multilingual support
+
+**Available Modules**
+
+The following modules are available in Slimpack:
+
+* Carousels
+* Contact Form
+* Custom CSS
+* Infinite Scroll
+* Beautiful Math (LaTeX)
+* Markdown
+* Shortcode Embeds
+* Site Icon
+* Site Verification
+* Widget Visibility
+* Widgets
+
+Additional note:
+
+* `devicepx-jetpack.js` is already packed in the plugin! No external script will slow down your website.
+* WordPress version check is missing in Slim version, so make sure your WordPress is up-to-date.
+* Beautiful Math: still need internet connection to get generated LaTeX images from `wp.com`.
+* Site Icon: Sync icon to WordPress.com via Jetpack server is disabled.
+
+**Under-the-hood Features**
+
+The following hidden modules are also available in Slimpack:
+
+* Holiday Snow module
+* Feature Content support
+* Other theme tools originally shipped from Jetpack
+
+**Work in Progress**
+
+The following modules are working-in-progress, I'll add them later.
+
+* Gravatar Hovercards
+* Omnisearch
+* Sharing Support
+* Mobile Theme (Minileven)
+* VaultPress
+
+**Dropped Modules**
+
+The following modules **should** be working in Slim version but…
+
+* Custom Content Types: It requires too many Jetpack dependencies.
+* Tiled Galleries: It requires Photon to retrieve images generated from WordPress.com proxy.
+
+**Unsupported Modules**
+
+The following modules in Jetpack are not supported in Slimpack.
+
+* Enhanced Distribution
+* JSON API
+* Jetpack Comments
+* Jetpack Single Sign On
+* Likes
+* Manage
+* Monitor
+* Notifications
+* Photon
+* Post by Email
+* Protect
+* Publicize
+* Related Posts
+* Spelling and Grammar
+* Subscriptions
+* VideoPress
+* WP.me Shortlinks
+* WordPress.com Stats
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+WordPress:
+
+1. Upload the extracted files to the `/wp-content/plugins/` directory
+2. In 'Plugins' page, choose 'Activate'
+
+Multisite enabled WordPress:
+
+1. Upload the extracted files to the `/wp-content/plugins/` directory
+2. In 'Site Admin' mode (You need log in as site admin), go to 'Plugins' page, choose 'Network Activate'
+3. Just FYI, you can also activate this plugin individually for different sites.
+
+Old-styled WordPress Mu:
+
+1. Upload the extracted files to the `/wp-content/mu-plugins/` directory
+2. That's all.
+
+== Frequently Asked Questions ==
+
+= Is original Jepack required for this plugin? =
+
+No.
+
+= Can I use this with original Jetpack at the same time? =
+
+No. It's not compatible with the original Jetpack. You can't activate Jetpack and Slimpack at the same time.
+
+== Screenshots ==
+
+1. A great demo
+
+2. Option page
+
+== Changelog ==
+
+= 1.0.0 =
+* First release
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+* First release
---
+++ slimpack.php
@@ -0,0 +1,318 @@
+<?php
+/*
+Plugin Name: Slimpack
+Plugin URI: http://sparanoid.com/work/slimpack/
+Description: Slimpack — Lightweight Jetpack. Super-fast performance without modules that requires contracting WordPress.com.
+Version: 1.0.0
+Author: Tunghsiao Liu
+Author URI: http://sparanoid.com/
+Author Email: t@sparanoid.com
+Text Domain: slimpack
+Domain Path: /languages/
+Network: false
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+	Copyright 2014 Tunghsiao Liu (t@sparanoid.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA	02110-1301	USA
+
+*/
+
+define( 'SLIMPACK__BASE',							'slimpack/' );
+define( 'JETPACK__PLUGIN_DIR',				plugin_dir_path( __FILE__ ) . SLIMPACK__BASE );
+define( 'JETPACK__PLUGIN_FILE',				__FILE__ );
+
+if ( !class_exists( 'Jetpack' ) ) {
+	require_once( JETPACK__PLUGIN_DIR . 'class.jetpack.php' );
+}
+
+if ( !function_exists( 'jetpack_is_mobile' ) ) {
+	require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-user-agent.php' );
+}
+
+if ( !class_exists( 'Jetpack_Options' ) ) {
+	require_once( JETPACK__PLUGIN_DIR . 'class.jetpack-options.php' );
+}
+
+if ( !function_exists( 'jetpack_require_lib' ) ) {
+	require_once( JETPACK__PLUGIN_DIR . 'require-lib.php' );
+}
+
+register_activation_hook( __FILE__, 'slimpack_add_defaults' );
+register_uninstall_hook( __FILE__, 'slimpack_delete_plugin_options' );
+add_action( 'init', 'slimpack_i18n_init' );
+add_action( 'admin_init', 'slimpack_init' );
+add_action( 'admin_menu', 'slimpack_add_options_page' );
+add_action( 'plugins_loaded', 'slimpack_conditions' );
+add_filter( 'plugin_action_links', 'slimpack_plugin_action_links', 10, 2 );
+
+// Slimpack
+add_action( 'init', array( 'Jetpack', 'init' ) );
+add_action( 'plugins_loaded', array( 'Jetpack', 'load_modules' ), 100 );
+
+add_action( 'activated_plugin','slimpack_save_error' );
+function slimpack_save_error(){
+		update_option( 'sparanoid_plugin_error',	ob_get_contents() );
+}
+
+add_action( 'shutdown','slimpack_show_plugin_error' );
+function slimpack_show_plugin_error(){
+		echo get_option('plugin_error');
+}
+
+/**
+ * Admin Options
+ *
+ * @since Slimpack 1.1.0
+ */
+
+// Delete plugin created table when plugin deleted
+function slimpack_delete_plugin_options() {
+	delete_option('slimpack_options');
+}
+
+// Default option settings
+function slimpack_add_defaults() {
+
+	$tmp = get_option('slimpack_options');
+
+	if( ( (isset($tmp['chk_default_options_db']) && $tmp['G']=='1')) || (!is_array($tmp)) ) {
+		$arr = array(
+			"jp_carousel" => "1",
+			"jp_contact_form" => "1",
+			"jp_custom_css" => "1",
+			"jp_infinite_scroll" => "1",
+			"jp_latex" => "0",
+			"jp_markdown" => "1",
+			"jp_shortcodes" => "1",
+			"jp_site_icon" => "1",
+			"jp_verification_tools" => "1",
+			"jp_widget_visibility" => "1",
+			"jp_widgets" => "1",
+			"radio_strict_filtering" => "strict_on"
+		);
+		update_option('slimpack_options', $arr);
+	}
+}
+
+// Load the plugin text domain for translation
+function slimpack_i18n_init() {
+	load_plugin_textdomain( 'slimpack', false, dirname( plugin_basename( JETPACK__PLUGIN_FILE ) ) . '/languages/' );
+}
+
+// Initialized options to white list our options
+function slimpack_init() {
+
+	// Checks radio buttons have a valid choice (ie. no section is blank)
+	// Primarily to check newly added options have correct initial values
+	$tmp = get_option('slimpack_options');
+
+	// Check strict filtering option has a starting value
+	if(!$tmp['radio_strict_filtering']) {
+		$tmp["radio_strict_filtering"] = "strict_off";
+		update_option('slimpack_options', $tmp);
+	}
+
+	// Register settings
+	register_setting( 'slimpack_plugin_options', 'slimpack_options' );
+}
+
+// Menu page
+function slimpack_add_options_page() {
+	add_options_page(
+		__( 'Slimpack', 'slimpack' ),
+		__( 'Slimpack', 'slimpack' ),
+		'manage_options', 'slimpack', 'slimpack_render_form'
+	);
+}
+
+// Menu page content
+function slimpack_render_form() {
+	?>
+	<div class="wrap">
+		<div class="icon32" id="icon-options-general"><br></div>
+		<h2><?php _e( 'Slimpack Options', 'slimpack' ); ?></h2>
+		<!-- <h3>Slimpack Options</h3> -->
+		<p><?php _e( 'Slimpack — Lightweight Jetpack. Super-fast performance without modules that requires contracting WordPress.com.', 'slimpack' ); ?></p>
+
+		<form method="post" action="options.php">
+			<?php settings_fields('slimpack_plugin_options'); ?>
+			<?php $options = get_option('slimpack_options'); ?>
+			<table class="form-table">
+				<tr valign="top">
+					<th scope="row"><?php _e( 'Active Jetpack Modules', 'slimpack' ); ?></th>
+					<td>
+						<fieldset>
+							<label>
+								<input name="slimpack_options[jp_carousel]" type="checkbox" value="1" <?php if (isset($options['jp_carousel'])) { checked('1', $options['jp_carousel']); } ?>>
+								<?php _e( 'Carousel', 'jetpack' ); ?>
+							</label><br>
+
+							<label>
+								<input name="slimpack_options[jp_contact_form]" type="checkbox" value="1" <?php if (isset($options['jp_contact_form'])) { checked('1', $options['jp_contact_form']); } ?>>
+								<?php _e( 'Contact Form', 'jetpack' ); ?>
+							</label><br>
+
+							<label>
+								<input name="slimpack_options[jp_custom_css]" type="checkbox" value="1" <?php if (isset($options['jp_custom_css'])) { checked('1', $options['jp_custom_css']); } ?>>
+								<?php _e( 'Custom CSS', 'jetpack' ); ?>
+							</label><br>
+
+							<label>
+								<input name="slimpack_options[jp_infinite_scroll]" type="checkbox" value="1" <?php if (isset($options['jp_infinite_scroll'])) { checked('1', $options['jp_infinite_scroll']); } ?>>
+								<?php _e( 'Infinite Scroll', 'slimpack' ); ?>
+							</label><br>
+
+							<label>
+								<input name="slimpack_options[jp_latex]" type="checkbox" value="1" <?php if (isset($options['jp_latex'])) { checked('1', $options['jp_latex']); } ?>>
+								<?php _e( 'Beautiful Math', 'slimpack' ); ?>
+							</label><br>
+
+							<label>
+								<input name="slimpack_options[jp_markdown]" type="checkbox" value="1" <?php if (isset($options['jp_markdown'])) { checked('1', $options['jp_markdown']); } ?>>
+								<?php _e( 'Markdown', 'jetpack' ); ?>
+							</label><br>
+
+							<label>
+								<input name="slimpack_options[jp_shortcodes]" type="checkbox" value="1" <?php if (isset($options['jp_shortcodes'])) { checked('1', $options['jp_shortcodes']); } ?>>
+								<?php _e( 'Shortcode Embeds', 'jetpack' ); ?>
+							</label><br>
+
+							<label>
+								<input name="slimpack_options[jp_site_icon]" type="checkbox" value="1" <?php if (isset($options['jp_site_icon'])) { checked('1', $options['jp_site_icon']); } ?>>
+								<?php _e( 'Site Icon', 'jetpack' ); ?>
+							</label><br>
+
+							<label>
+								<input name="slimpack_options[jp_verification_tools]" type="checkbox" value="1" <?php if (isset($options['jp_verification_tools'])) { checked('1', $options['jp_verification_tools']); } ?>>
+								<?php _e( 'Site Verification', 'slimpack' ); ?>
+							</label><br>
+
+							<label>
+								<input name="slimpack_options[jp_widget_visibility]" type="checkbox" value="1" <?php if (isset($options['jp_widget_visibility'])) { checked('1', $options['jp_widget_visibility']); } ?>>
+								<?php _e( 'Widget Visibility', 'slimpack' ); ?>
+							</label><br>
+
+							<label>
+								<input name="slimpack_options[jp_widgets]" type="checkbox" value="1" <?php if (isset($options['jp_widgets'])) { checked('1', $options['jp_widgets']); } ?>>
+								<?php _e( 'Widgets', 'slimpack' ); ?>
+							</label><br>
+						</fieldset>
+					</td>
+				</tr>
+			</table>
+			<p class="submit">
+				<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>">
+			</p>
+			<hr>
+			<p><?php _e( 'Love this plugin? Please consider', 'slimpack' ); ?> <a href="http://sparanoid.com/donate/"><?php _e( 'buying me a cup of coffee!', 'slimpack' ); ?></a></p>
+			<p>
+				<input class="button" type="button" value="<?php _e( 'Follow on Twitter', 'slimpack' ); ?>" onClick="window.open('http://twitter.com/tunghsiao')">
+				<input class="button" type="button" value="<?php _e( 'Visit My Website', 'slimpack' ); ?>" onClick="window.open('http://sparanoid.com/')">
+				<input class="button" type="button" value="<?php _e( 'View plugin at WordPress.org', 'slimpack' ); ?>" onClick="window.open('https://wordpress.org/plugins/slimpack/')">
+			</p>
+		</form>
+	</div>
+	<?php
+}
+
+/**
+ * Plugin Functions
+ *
+ * @since Slimpack 1.1.0
+ */
+
+function slimpack_conditions() {
+
+	$tmp = get_option('slimpack_options');
+
+	if (isset($tmp['jp_carousel'])) {
+		if($tmp['jp_carousel']=='1'){
+			require_once( JETPACK__PLUGIN_DIR . 'modules/carousel.php' );
+		}
+	}
+
+	if (isset($tmp['jp_contact_form'])) {
+		if($tmp['jp_contact_form']=='1'){
+			require_once( JETPACK__PLUGIN_DIR . 'modules/contact-form.php' );
+		}
+	}
+
+	if (isset($tmp['jp_custom_css'])) {
+		if($tmp['jp_custom_css']=='1'){
+			require_once( JETPACK__PLUGIN_DIR . 'modules/custom-css.php' );
+		}
+	}
+
+	if (isset($tmp['jp_infinite_scroll'])) {
+		if($tmp['jp_infinite_scroll']=='1'){
+			require_once( JETPACK__PLUGIN_DIR . 'modules/infinite-scroll.php' );
+		}
+	}
+
+	if (isset($tmp['jp_latex'])) {
+		if($tmp['jp_infinite_scroll']=='1'){
+			require_once( JETPACK__PLUGIN_DIR . 'modules/latex.php' );
+		}
+	}
+
+	if (isset($tmp['jp_markdown'])) {
+		if($tmp['jp_markdown']=='1'){
+			require_once( JETPACK__PLUGIN_DIR . 'modules/markdown.php' );
+		}
+	}
+
+	if (isset($tmp['jp_shortcodes'])) {
+		if($tmp['jp_shortcodes']=='1'){
+			require_once( JETPACK__PLUGIN_DIR . 'modules/shortcodes.php' );
+		}
+	}
+
+	if (isset($tmp['jp_site_icon'])) {
+		if($tmp['jp_site_icon']=='1'){
+			require_once( JETPACK__PLUGIN_DIR . 'modules/site-icon.php' );
+		}
+	}
+
+	if (isset($tmp['jp_verification_tools'])) {
+		if($tmp['jp_verification_tools']=='1'){
+			require_once( JETPACK__PLUGIN_DIR . 'modules/verification-tools.php' );
+		}
+	}
+
+	if (isset($tmp['jp_widget_visibility'])) {
+		if($tmp['jp_widget_visibility']=='1'){
+			require_once( JETPACK__PLUGIN_DIR . 'modules/widget-visibility.php' );
+		}
+	}
+
+	if (isset($tmp['jp_widgets'])) {
+		if($tmp['jp_widgets']=='1'){
+			require_once( JETPACK__PLUGIN_DIR . 'modules/widgets.php' );
+		}
+	}
+}
+
+// Add a 'Settings' link on Plugins page
+function slimpack_plugin_action_links( $links, $file ) {
+
+	if ( $file == plugin_basename( __FILE__ ) ) {
+		$posk_links = '<a href="'.get_admin_url().'options-general.php?page=slimpack">'.__('Settings').'</a>';
+		// Make sure the 'Settings' link at first
+		array_unshift( $links, $posk_links );
+	}
+
+	return $links;
+}
